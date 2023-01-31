5c0096a04e9d40c35f53686bebb515a66f0ff6b8
Caio Vinícius <caio1525pereira@gmail.com>
2023-01-31 18:24:21 -0300

    feat: :sparkles: Endpoint de criação de hábito

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,34 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import dayjs from "dayjs";
+
+import { prisma } from "./lib/prisma";
 
 export async function appRoutes(app: FastifyInstance) {
-	app.get("/", () => {
-		return "Hello, world!";
+	app.post("/habits", async (req, res) => {
+		const createHabitBody = z.object({
+			title: z.string(),
+			weekDays: z.array(z.number().min(0).max(6))
+		});
+
+		const { title, weekDays } = createHabitBody.parse(req.body);
+
+		const today = dayjs().startOf("day").toDate();
+
+		await prisma.habits.create({
+			data: {
+				title,
+				created_at: today,
+				weekDays: {
+					create: weekDays.map((weekDay) => {
+						return {
+							week_day: weekDay
+						};
+					})
+				}
+			}
+		});
+
+		return res.status(201).send();
 	});
 }
